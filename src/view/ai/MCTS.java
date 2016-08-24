fc8c48b994263916a8c46534b41bfb2bdb95ed92
DaeVo <m.roth1991@yahoo.de>
2016-08-24 02:31:26 +0200

    continueing on move methods for the AI

--- src/view/ai/MCTS.java
+++ src/view/ai/MCTS.java
@@ -1,14 +1,10 @@
 package view.ai;
-
 import model.Gamestate;
-import model.Playfield;
-
 import java.awt.Point;
 import java.awt.Color;
 import java.util.LinkedList;
 import java.util.Random;
 import java.util.List;
-import java.util.Set;
 
 /**
  * Created by Max on 19/08/2016.
@@ -19,7 +15,7 @@ public class MCTS {
 
 
     public Node root; //global root Node
-    Gamestate gametateObject = new Gamestate(null);
+
     /*
     initializes the tree
      */
@@ -27,52 +23,11 @@ public class MCTS {
         root = new Node();
     }
 
-    public int getRandomNumber(){
-        Random rn = new Random();
-        return rn.nextInt();
-    }
-    public Color color;
-
-    private Point randomMoveSource () { //selects a random piece to move this round
-        List<Point> tmpList = new LinkedList<Point>();
-        for (model.Pieces p : gametateObject.currentPieces) {
-            if (color == p.color) {
-                Point tmpPoint = new Point (p.field.x, p.field.y);
-                tmpList.add(tmpPoint);
-            }
-        }
-        return tmpList.get(getRandomNumber() % tmpList.size());
-    }
-
-    private Point selectRandomPlacing() { //random move while placing Pieces
-        //place here already?
-        return gametateObject.legalPlacing.get(getRandomNumber() % gametateObject.legalPlacing.size());
-    }
-
-    private Move selectRandomMove() { //random move while moving with more than 3 Pieces.
-        Point tmpSrc = randomMoveSource();
-        List<Point> dstList = gametateObject.legalMoves.get(tmpSrc);
-        //perform move here already?
-        return new Move(tmpSrc, dstList.get(getRandomNumber() % dstList.size()));
-    }
-
-    private Point selectRandomRemove() { //randomly selected field of an enemy field to remove the piece.
-        List<Point> enemyPieces = new LinkedList<Point>();
-        for (model.Pieces p : gametateObject.currentPieces) {
-            if (p.color != color) {
-                Point tmpPoint = new Point(p.field.x, p.field.y);
-                enemyPieces.add(tmpPoint);
-            }
-        }
-        //conquer field here already?
-        return enemyPieces.get(getRandomNumber() % enemyPieces.size());
-    }
-
     /*
     chose the best Node
      */
-    public void selectMove() {
-        //logic to select e.g. highest winrate or highest win count node
+    public void selectMove() { //finally decides for a move and sets the root to the next move
+        //logic to select e.g. highest win rate or highest win count node
         // root = selectedNode
     }
 }
--- src/view/ai/SmartAi.java
+++ src/view/ai/SmartAi.java
@@ -1,5 +1,6 @@
 package view.ai;
 
+import model.Gamestate;
 import model.Pieces;
 import model.Playfield;
 import view.AbstractPlayer;
@@ -7,8 +8,12 @@ import view.AbstractPlayer;
 import java.awt.*;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 public class SmartAi extends AbstractPlayer {
+
+    Gamestate gametateObject = new Gamestate(null);
+
     @Override
     public void run() {
         System.out.println("Ki: run()");
@@ -37,5 +42,49 @@ public class SmartAi extends AbstractPlayer {
     }
 
     private void removeStone() {
+    }
+
+
+
+
+    public int getRandomNumber(){
+        Random rn = new Random();
+        return rn.nextInt();
+    }
+    public Color color;
+
+    private Point randomMoveSource () { //selects a random piece to move this round
+        List<Point> tmpList = new LinkedList<Point>();
+        for (model.Pieces p : gametateObject.currentPieces) {
+            if (color == p.color) {
+                Point tmpPoint = new Point (p.field.x, p.field.y);
+                tmpList.add(tmpPoint);
+            }
+        }
+        return tmpList.get(getRandomNumber() % tmpList.size());
+    }
+
+    private Point selectRandomPlacing() { //random move while placing Pieces
+        //place here already?
+        return gametateObject.legalPlacing.get(getRandomNumber() % gametateObject.legalPlacing.size());
+    }
+
+    private Move selectRandomMove() { //random move while moving with more than 3 Pieces.
+        Point tmpSrc = randomMoveSource();
+        List<Point> dstList = gametateObject.legalMoves.get(tmpSrc);
+        //perform move here already?
+        return new Move(tmpSrc, dstList.get(getRandomNumber() % dstList.size()));
+    }
+
+    private Point selectRandomRemove() { //randomly selected field of an enemy field to remove the piece.
+        List<Point> enemyPieces = new LinkedList<Point>();
+        for (model.Pieces p : gametateObject.currentPieces) {
+            if (p.color != color) {
+                Point tmpPoint = new Point(p.field.x, p.field.y);
+                enemyPieces.add(tmpPoint);
+            }
+        }
+        //conquer field here already?
+        return enemyPieces.get(getRandomNumber() % enemyPieces.size());
     }
 }
